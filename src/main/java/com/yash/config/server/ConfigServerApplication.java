503dcd0dd0846ade2415399b50976dab08b99a0b
kamra.gaurav <gauravjava1206@gmail.com>
2023-03-27 23:10:08 +0530

    config server implementation 2023

--- src/main/java/com/yash/config/server/ConfigServerApplication.java
+++ src/main/java/com/yash/config/server/ConfigServerApplication.java
@@ -2,10 +2,13 @@ package com.yash.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaClient
+
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
